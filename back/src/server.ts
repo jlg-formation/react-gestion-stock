89d5ca6e454c2957414cb3e62b75681351a36fc8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-19 14:43:17 +0100

    dev: make SSR work also on url /

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,6 @@
 console.log("About to start a server");
 
-import express from "express";
+import express, { Request, Response } from "express";
 import fs from "fs/promises";
 import serveIndex from "serve-index";
 import { pathToFileURL } from "url";
@@ -10,6 +10,17 @@ const app = express();
 const port = +(process.env.GSTOCK_PORT || 3000);
 const publicDir = "../front/dist/client";
 const serverDir = "../front/dist/server";
+
+const ssrMiddleware = (req: Request, res: Response) => {
+  (async () => {
+    const template = await fs.readFile(publicDir + "/index.html", "utf-8");
+    const serverFile = pathToFileURL(serverDir + "/entry-server.js").toString();
+    const render = (await import(serverFile)).render;
+    const appHtml = await render(req);
+    const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+    res.status(200).send(html);
+  })();
+};
 
 app.use((req, res, next) => {
   console.log("req: ", req.method, req.url);
@@ -25,19 +36,12 @@ app.use((req, res, next) => {
 
 app.use("/api", api);
 
+app.get("/", ssrMiddleware);
+
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
 
-app.use("*", (req, res) => {
-  (async () => {
-    const template = await fs.readFile(publicDir + "/index.html", "utf-8");
-    const serverFile = pathToFileURL(serverDir + "/entry-server.js").toString();
-    const render = (await import(serverFile)).render;
-    const appHtml = await render(req);
-    const html = template.replace(`<!--ssr-outlet-->`, appHtml);
-    res.status(200).send(html);
-  })();
-});
+app.use("*", ssrMiddleware);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
