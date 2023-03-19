cd345b1c8ee42c36853af542839d8b535e2230a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-19 10:23:47 +0100

    dev: use import.meta.env.SSR
    - Legal page : print if the component was
      generated on the Server (SSR) or Client

--- front/src/router/views/LegalView.tsx
+++ front/src/router/views/LegalView.tsx
@@ -1,71 +1,76 @@
-const LegalView = () => (
-  <main>
-    <h1>Mentions Légales</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
-      quam quia minima velit accusamus earum, sit unde quidem molestiae eligendi
-      tempora quos officiis, incidunt at eum nesciunt nostrum est.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-      praesentium laudantium optio dolor quas nihil, in cumque dolores. Tempora
-      possimus, doloribus repellat recusandae consequuntur dolor a, nihil sit
-      eius, quis repudiandae magni. Sapiente cupiditate possimus nulla fugit
-      repellendus recusandae, accusantium nobis et quo, tempore perspiciatis
-      explicabo? Consequatur distinctio accusamus quos.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
-      quam quia minima velit accusamus earum, sit unde quidem molestiae eligendi
-      tempora quos officiis, incidunt at eum nesciunt nostrum est.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-      praesentium laudantium optio dolor quas nihil, in cumque dolores. Tempora
-      possimus, doloribus repellat recusandae consequuntur dolor a, nihil sit
-      eius, quis repudiandae magni. Sapiente cupiditate possimus nulla fugit
-      repellendus recusandae, accusantium nobis et quo, tempore perspiciatis
-      explicabo? Consequatur distinctio accusamus quos.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
-      quam quia minima velit accusamus earum, sit unde quidem molestiae eligendi
-      tempora quos officiis, incidunt at eum nesciunt nostrum est.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-      praesentium laudantium optio dolor quas nihil, in cumque dolores. Tempora
-      possimus, doloribus repellat recusandae consequuntur dolor a, nihil sit
-      eius, quis repudiandae magni. Sapiente cupiditate possimus nulla fugit
-      repellendus recusandae, accusantium nobis et quo, tempore perspiciatis
-      explicabo? Consequatur distinctio accusamus quos.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
-      quam quia minima velit accusamus earum, sit unde quidem molestiae eligendi
-      tempora quos officiis, incidunt at eum nesciunt nostrum est.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-      praesentium laudantium optio dolor quas nihil, in cumque dolores. Tempora
-      possimus, doloribus repellat recusandae consequuntur dolor a, nihil sit
-      eius, quis repudiandae magni. Sapiente cupiditate possimus nulla fugit
-      repellendus recusandae, accusantium nobis et quo, tempore perspiciatis
-      explicabo? Consequatur distinctio accusamus quos.
-    </p>
-  </main>
-)
+const ssrLabel = import.meta.env.SSR ? 'SSR' : 'Client'
+
+const LegalView = () => {
+  return (
+    <main>
+      <h1>Mentions Légales</h1>
+      <span>Source : {ssrLabel}</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
+        quam quia minima velit accusamus earum, sit unde quidem molestiae
+        eligendi tempora quos officiis, incidunt at eum nesciunt nostrum est.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        praesentium laudantium optio dolor quas nihil, in cumque dolores.
+        Tempora possimus, doloribus repellat recusandae consequuntur dolor a,
+        nihil sit eius, quis repudiandae magni. Sapiente cupiditate possimus
+        nulla fugit repellendus recusandae, accusantium nobis et quo, tempore
+        perspiciatis explicabo? Consequatur distinctio accusamus quos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
+        quam quia minima velit accusamus earum, sit unde quidem molestiae
+        eligendi tempora quos officiis, incidunt at eum nesciunt nostrum est.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        praesentium laudantium optio dolor quas nihil, in cumque dolores.
+        Tempora possimus, doloribus repellat recusandae consequuntur dolor a,
+        nihil sit eius, quis repudiandae magni. Sapiente cupiditate possimus
+        nulla fugit repellendus recusandae, accusantium nobis et quo, tempore
+        perspiciatis explicabo? Consequatur distinctio accusamus quos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
+        quam quia minima velit accusamus earum, sit unde quidem molestiae
+        eligendi tempora quos officiis, incidunt at eum nesciunt nostrum est.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        praesentium laudantium optio dolor quas nihil, in cumque dolores.
+        Tempora possimus, doloribus repellat recusandae consequuntur dolor a,
+        nihil sit eius, quis repudiandae magni. Sapiente cupiditate possimus
+        nulla fugit repellendus recusandae, accusantium nobis et quo, tempore
+        perspiciatis explicabo? Consequatur distinctio accusamus quos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolorem
+        quam quia minima velit accusamus earum, sit unde quidem molestiae
+        eligendi tempora quos officiis, incidunt at eum nesciunt nostrum est.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, neque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        praesentium laudantium optio dolor quas nihil, in cumque dolores.
+        Tempora possimus, doloribus repellat recusandae consequuntur dolor a,
+        nihil sit eius, quis repudiandae magni. Sapiente cupiditate possimus
+        nulla fugit repellendus recusandae, accusantium nobis et quo, tempore
+        perspiciatis explicabo? Consequatur distinctio accusamus quos.
+      </p>
+    </main>
+  )
+}
 
 export default LegalView
